aab882fda5a4d6ae8925e786d95e7a6ffea61b50
bichuaaditya-byte <bichuaaditya@gmail.com>
2026-08-18 17:34:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,51 @@
 const USERS = [
-{id:"aaditya",name:"Aaditya",icon:"👤",subjects:[{id:"vlsi",name:"VLSI",icon:"🔌"},{id:"arm",name:"ARM",icon:"⚙️"},{id:"os",name:"OS",icon:"💿"},{id:"coa",name:"COA",icon:"💻"},{id:"instrumentation",name:"Instrumentation",icon:"🤖"}]},
-{id:"remmy",name:"Remmy",icon:"👤",subjects:[{id:"digital-electronics",name:"Digital Electronics",icon:"🔬"},{id:"java",name:"Java",icon:"☕"}]}
+    {
+        id: "aaditya",
+        name: "Aaditya",
+        icon: "👤",
+        subjects: [
+            {
+                id: "vlsi",
+                name: "VLSI",
+                icon: "🔌"
+            },
+            {
+                id: "arm",
+                name: "ARM",
+                icon: "⚙️"
+            },
+            {
+                id: "os",
+                name: "OS",
+                icon: "💿"
+            },
+            {
+                id: "coa",
+                name: "COA",
+                icon: "💻"
+            },
+            {
+                id: "instrumentation",
+                name: "Instrumentation",
+                icon: "🤖"
+            }
+        ]
+    },
+    {
+        id: "remmy",
+        name: "Remmy",
+        icon: "👤",
+        subjects: [
+            {
+                id: "digital-electronics",
+                name: "Digital Electronics",
+                icon: "🔬"
+            },
+            {
+                id: "java",
+                name: "Java",
+                icon: "☕"
+            }
+        ]
+    }
 ];
