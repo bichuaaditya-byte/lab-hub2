564b074f058db09bf588eaaf694925af9126f1d0
bichuaaditya-byte <bichuaaditya@gmail.com>
2026-08-18 17:29:35 +0530

    Add files via upload

---
+++ config[1].js
@@ -0,0 +1,4 @@
+const USERS = [
+{id:"aaditya",name:"Aaditya",icon:"👤",subjects:[{id:"vlsi",name:"VLSI",icon:"🔌"},{id:"arm",name:"ARM",icon:"⚙️"},{id:"os",name:"OS",icon:"💿"},{id:"coa",name:"COA",icon:"💻"},{id:"instrumentation",name:"Instrumentation",icon:"🤖"}]},
+{id:"remmy",name:"Remmy",icon:"👤",subjects:[{id:"digital-electronics",name:"Digital Electronics",icon:"🔬"},{id:"java",name:"Java",icon:"☕"}]}
+];
